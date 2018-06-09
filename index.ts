fb08eefb0d836ea81054c1f57028e9d0b9fb0380
Zheng Hanming <hanming2033@gmail.com>
2018-06-09 21:18:03 +0800

    a

--- index.ts
+++ index.ts
@@ -3,46 +3,72 @@
 import * as http from 'http'
 import * as url from 'url'
 
-// how http works
-// 1. define what the server does with http module
-// each time a request comes in, server will call the function with req and res object filled in
-// req is a bunch of info about what user is asking for e.g. url
-// res will be the object which returns by the server. this method manipulate the object
+import { StringDecoder } from 'string_decoder'
+
 const server = http.createServer((req: http.ServerRequest, res: http.ServerResponse) => {
-  if (!req || !req.url) return
-  // url lib is to parse url
-  // 1.get url and parse it
-  // true: send the url to the query string module which returns url object
+  if (!req || !req.url || !req.method) return
   const parsedUrl = url.parse(req.url, true)
 
-  // 2.get path from url
-  // returns the untrimmed path that the user requested 
-  // path without the tld
   const path = parsedUrl.pathname
-  // trim off the beining and ending slashes
-  // localhost:3000/hello/world => hello/world
+
   const trimmedPath = path ? path.replace(/^\/+|\/+$/g, '') : ''
 
-  // get the query string as an object
-  // disect query from url
-  // localhost:3000/query?hello=world => { hello: 'world' }
   const qryStrObj = parsedUrl.query
 
-  if (!req.method) return
-  // get http method: get, post, put ...
   const method = req.method.toLocaleLowerCase()
 
-  // TODO: continue headers
-  // get headers as an object
   const headers = req.headers
 
-  // 3.send response
-  res.end('hello world\n')
+  const decoder = new StringDecoder('utf8')
+  let stream = ''
+  req.on('data', (data: Buffer) => {
+    stream += decoder.write(data)
+  })
 
-  // 4.log the request path
-  console.log(headers)
-  console.log(method)
-  console.log(qryStrObj)
+  req.on('end', () => {
+    stream += decoder.end()
+
+    // choose the resolver this request should go to, default is notFound
+    const chosenResolver: hanlder = router[trimmedPath] ? router[trimmedPath] : router.notFound
+    // construct data object(from the path, headers... derived above) to send to the resolver
+    const data = { trimmedPath, qryStrObj, method, headers, payload: stream }
+    // route the request to the resolver specified in the router
+    chosenResolver(data, (statusCode, result) => {
+      // concept of this resolver:
+      // 1. pass in the data into resolver
+      // 2. resolver process the data and call this callback with statusCode and result
+      // 2. this callback simply converts the result into json and return statusCode and json
+
+      // convert the result into a string
+      // result here is the result we send to user
+      const resultStr = JSON.stringify(result)
+      // return the response
+      res.writeHead(statusCode)
+      res.end(resultStr)
+      console.log('returning this response : ', statusCode, resultStr)
+    })
+  })
 })
+
 // 2. tell server to listen on a port
 server.listen(3000, () => console.log('Server is listening on 3000'))
+
+// Router: match incoming path to some hanlders
+// define a request router & resolvers
+const router: IRouter = {
+  sample: (data, returnResultFn) => {
+    console.log(data)
+    // callback a http status code, and a result object
+    returnResultFn(200, { name: 'sample resolver' })
+  },
+  notFound: (data, returnResultFn) => {
+    console.log(data)
+    returnResultFn(404, {})
+  }
+}
+
+type cbReturnResultFn = (statusCode: number, result: { [key: string]: any }) => void
+type hanlder = (data: any, returnResultFn: cbReturnResultFn) => void
+interface IRouter {
+  [key: string]: hanlder
+}
