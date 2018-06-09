e7f279fbd2c1cc3b0b56f54b30aeb19702d1704a
hanming <hanming2033@gmail.com>
2018-06-09 22:53:40 +0800

    removed callback

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import * as http from 'http'
 import * as url from 'url'
 
 import { StringDecoder } from 'string_decoder'
+import { ParsedUrlQuery } from 'querystring'
 
 const server = http.createServer((req: http.ServerRequest, res: http.ServerResponse) => {
   if (!req || !req.url || !req.method) return
@@ -29,46 +30,53 @@ const server = http.createServer((req: http.ServerRequest, res: http.ServerRespo
     stream += decoder.end()
 
     // choose the resolver this request should go to, default is notFound
-    const chosenResolver: hanlder = router[trimmedPath] ? router[trimmedPath] : router.notFound
+    const resolve: resolver = router[trimmedPath] ? router[trimmedPath] : router.notFound
     // construct data object(from the path, headers... derived above) to send to the resolver
-    const data = { trimmedPath, qryStrObj, method, headers, payload: stream }
+    const data: IDataFromReq = { trimmedPath, qryStrObj, method, headers, payload: stream }
     // route the request to the resolver specified in the router
-    chosenResolver(data, (statusCode, result) => {
-      // concept of this resolver:
-      // 1. pass in the data into resolver
-      // 2. resolver process the data and call this callback with statusCode and result
-      // 2. this callback simply converts the result into json and return statusCode and json
-
-      // convert the result into a string
-      // result here is the result we send to user
-      const resultStr = JSON.stringify(result)
-      // return the response
-      res.writeHead(statusCode)
-      res.end(resultStr)
-      console.log('returning this response : ', statusCode, resultStr)
-    })
+    const { statusCode, result } = resolve(data)
+    // convert the result into a string
+    // result here is the result we send to user
+    const resultStr = JSON.stringify(result)
+    // return the response: header & result
+    res.setHeader('Content-Type','application/json')
+    res.writeHead(statusCode)
+    res.end(resultStr)
+    console.log('returning this response : ', statusCode, resultStr)
   })
 })
 
-// 2. tell server to listen on a port
 server.listen(3000, () => console.log('Server is listening on 3000'))
 
 // Router: match incoming path to some hanlders
 // define a request router & resolvers
 const router: IRouter = {
-  sample: (data, returnResultFn) => {
+  sample: data => {
     console.log(data)
-    // callback a http status code, and a result object
-    returnResultFn(200, { name: 'sample resolver' })
+    return { statusCode: 200, result: { name: 'sample resolver' } }
   },
-  notFound: (data, returnResultFn) => {
+  notFound: data => {
     console.log(data)
-    returnResultFn(404, {})
+    return { statusCode: 404, result: {} }
   }
 }
 
-type cbReturnResultFn = (statusCode: number, result: { [key: string]: any }) => void
-type hanlder = (data: any, returnResultFn: cbReturnResultFn) => void
+// interfaces
+interface IDataFromReq {
+  trimmedPath: string
+  qryStrObj: ParsedUrlQuery
+  method: string
+  headers: http.IncomingHttpHeaders
+  payload: string
+}
+
+interface IResults {
+  statusCode: number
+  result: { [key: string]: any }
+}
+
+type resolver = (data: any) => IResults
+
 interface IRouter {
-  [key: string]: hanlder
+  [key: string]: resolver
 }
